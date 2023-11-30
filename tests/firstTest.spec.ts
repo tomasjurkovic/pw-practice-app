4aef61359c208c574e3e077e06581c78ba04379f
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-11-30 12:52:36 +0100

    added new test lines

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -3,6 +3,8 @@ import { test } from "@playwright/test";
 test.describe("test suite 1", () => {
   test("The first test", async ({ page }) => {
     await page.goto("http://localhost:4200/");
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
   });
   test("The second test", () => {});
 });
