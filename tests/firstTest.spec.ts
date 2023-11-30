6baa5518a784f999b863b7025786fe0c30ed2c0f
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-11-30 13:03:38 +0100

    same structure as for cypress project

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,15 +1,28 @@
 import { test } from "@playwright/test";
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+});
 
 test.describe("test suite 1", () => {
-  test("The first test", async ({ page }) => {
-    await page.goto("http://localhost:4200/");
+  test.beforeEach(async ({ page }) => {
     await page.getByText("Forms").click();
+  });
+  test("The first test", async ({ page }) => {
     await page.getByText("Form Layouts").click();
   });
-  test("The second test", () => {});
+  test("The second test", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
 });
 
 test.describe("test suite 2", () => {
-  test("The third test", () => {});
-  test("The fourth test", () => {});
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
+  test("The third test", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+  test("The fourth test", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
 });
