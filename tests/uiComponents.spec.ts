bdf17a4158f38671001f9058ee44d3a40b1602aa
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-12-04 10:54:07 +0100

    added test example for tooltips

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -139,4 +139,19 @@ test.describe("test suite 2", () => {
       if (color != "Corporate") await dropdownMenu.click();
     }
   });
+
+  test("tooltips", async ({ page }) => {
+    await page.getByText("Modal & Overlays").click();
+    await page.getByText("Tooltip").click();
+
+    const tooltipCard = page.locator("nb-card", {
+      hasText: "Tooltip Placements",
+    });
+
+    await tooltipCard.getByRole("button", { name: "Top" }).hover();
+    // page.getByRole('tooltip') // only works if you have a role tooltip created
+
+    const tooltip = await page.locator("nb-tooltip").textContent();
+    expect(tooltip).toEqual("This is a tooltip");
+  });
 });
