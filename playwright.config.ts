308aa098106575928aecc45229c14cb3a374d707
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2024-01-03 14:32:35 +0100

    Update playwright.config.ts
    deleted unused code and comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,51 +1,38 @@
 import { defineConfig, devices } from "@playwright/test";
 import type { TestOptions } from "./test-options";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 require("dotenv").config();
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig<TestOptions>({
-  testDir: "./tests",
+  timeout: 40000,
+  globalTimeout: 60000,
 
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  expect: {
+    timeout: 2000,
+  },
+  retries: 1,
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:4200/",
     // for other projects such as autoWaiting & dragAndDrop
     // which uses different URLS I created test-options file
     // and here I can define substitute of base URL / globalsQaURL:
     globalsQaURL: "http://www.globalsqa.com/draganddrop", // used value from dragANdDrop test
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    navigationTimeout: 5000,
-    // video: {
-    //   mode: "on",
-    //   size: {
-    //     width: 1920,
-    //     height: 1080,
-    //   },
-    // },
+    actionTimeout: 20000,
+    navigationTimeout: 25000,
+    video: {
+      mode: "off",
+      size: {
+        width: 1920,
+        height: 1080,
+      },
+    },
     // here I put possible video configuration if videos are needed
   },
 
-  /* Configure projects for major browsers */
   projects: [
     // here I can insert different enviroments with their baseURL like this:
     {
@@ -76,32 +63,5 @@ export default defineConfig<TestOptions>({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
