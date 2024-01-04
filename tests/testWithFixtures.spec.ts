94ed0f5199ad3f92303a2df1b49783afcffb7d0f
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2024-01-04 13:59:32 +0100

    added pageManager in fixtures, the way of init it

--- tests/testWithFixtures.spec.ts
+++ tests/testWithFixtures.spec.ts
@@ -1,25 +1,23 @@
 import { test } from "../test-options";
-import { PageManager } from "../page-objects/pageManager";
 import { faker } from "@faker-js/faker";
 
 test.describe("test suite 1", () => {
   test("Login with valid credentials via Using The Grid Form test", async ({
-    page,
+    pageManager,
   }) => {
-    const pm = new PageManager(page);
     const randomFullName = faker.person.fullName();
     const randomEmail = `${randomFullName.replace(" ", "")}${faker.number.int(
       1000
     )}@test.sk`;
 
-    await pm
+    await pageManager
       .onFormLayoutsPage()
       .submitUsingTheGridFormWithCredentialsAndSelectOption(
         randomFullName,
         "password",
         "Option 1"
       );
-    await pm
+    await pageManager
       .onFormLayoutsPage()
       .submitUsingTheGridFormWithCredentialsAndCheckbox(
         randomEmail,
