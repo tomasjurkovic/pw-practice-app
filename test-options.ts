53a9b65e71f0a21a7c8ff209488dacabf08e53ba
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2024-01-04 13:42:58 +0100

    formLayoutsPage fixture is created and used in real test

--- test-options.ts
+++ test-options.ts
@@ -2,8 +2,19 @@ import { test as base } from "@playwright/test";
 
 export type TestOptions = {
   globalsQaURL: string;
+  formLayoutPage: string;
 };
 
 export const test = base.extend<TestOptions>({
   globalsQaURL: ["", { option: true }],
+
+  formLayoutPage: [
+    async ({ page }, use) => {
+      await page.goto("/");
+      await page.getByText("Forms").click();
+      await page.getByText("Form Layouts").click();
+      await use("");
+    },
+    { auto: true },
+  ],
 });
