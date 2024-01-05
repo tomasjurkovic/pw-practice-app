426796deb89f1a238b1d361cfa34b6c452040575
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2024-01-05 10:26:30 +0100

    Update testMobile.spec.ts
    added missing step

--- tests/testMobile.spec.ts
+++ tests/testMobile.spec.ts
@@ -2,6 +2,7 @@ import { test } from "@playwright/test";
 
 test("Input fields test", async ({ page }) => {
   await page.goto("/");
+  await page.locator("g[data-name$='menu-2']").click();
   await page.getByText("Forms").click();
   await page.getByText("Form Layouts").first().click();
 
