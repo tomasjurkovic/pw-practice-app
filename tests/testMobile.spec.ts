c6304e21276afa13155eca81698e1edcf55d207f
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2024-01-05 09:51:03 +0100

    created new project for mobile devices

---
+++ tests/testMobile.spec.ts
@@ -0,0 +1,17 @@
+import { test } from "@playwright/test";
+
+test("Input fields test", async ({ page }) => {
+  await page.goto("/");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").first().click();
+
+  const usingTheGridEmailInput = page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" });
+
+  await usingTheGridEmailInput.fill("demo@email.com");
+  await usingTheGridEmailInput.clear();
+  await usingTheGridEmailInput.pressSequentially("test@new.com", {
+    delay: 500,
+  });
+});
